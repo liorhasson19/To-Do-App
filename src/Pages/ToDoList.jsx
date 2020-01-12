b05508decb1f906ca1523536b629633218faba80
Lior Hasson <liorhasson@gmail.com>
2020-01-12 10:04:28 +0200

    Created React Component

---
+++ src/Pages/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ToDoList extends Component {
+    render() {
+        return (
+            <div>
+              Hello  
+            </div>
+        )
+    }
+}
+
