fa89988386ed24af9e765fda78d8a67048fb885b
Lior Hasson <liorhasson@gmail.com>
2020-01-12 10:54:59 +0200

    Added input with onchange function

--- src/Pages/ToDoList.jsx
+++ src/Pages/ToDoList.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import { Container, Jumbotron, Button } from 'react-bootstrap'
+import { Container, Jumbotron, Row } from 'react-bootstrap'
 
 export default class ToDoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: ""
+        };
+    }
+
+    onChangeHandler = (ev) => {
+        this.setState({ input: ev.target.value });
+        console.log("this.state.input: " + this.state.input)
+    }
+
     render() {
+        const { input } = this.state;
+
         return (
             <div>
                 <Jumbotron>
                     <h1>ToDos</h1>
                 </Jumbotron>
-
+                <Container>
+                    <Row>
+                        <input className="filter-input" type="text" id="myInput" value={input}
+                            placeholder="What's next?" onChange={this.onChangeHandler} />
+                    </Row>
+                    <Row>
+                        <div className="myInput">{this.state.input}</div>
+                    </Row>
+                </Container>
             </div>
         )
     }
